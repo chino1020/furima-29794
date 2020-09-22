241476e86c9f3ecd962afd51464123e89acb2d54
chino1020 <70316634+chino1020@users.noreply.github.com>
2020-09-22 17:14:57 +0900

    サインインの分岐とバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, presence: true
 
+  validates :nickname, presence: true
+
+  with_options presence: true, format: { with: /¥A[一-龥]+¥z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /¥A[ァ-ン]+¥z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  VALID_PASSWORD_REGEX =/¥A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[¥d])¥w{6,}¥z/
+  validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX}
+
+  validates :birth_date, presence: true
 end
