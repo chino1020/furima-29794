d11bb28f2f39786bc3bc7d31e09a341f55184cef
chino1020 <70316634+chino1020@users.noreply.github.com>
2020-09-23 17:40:56 +0900

    ActiveHashでのプルダウンリストの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,6 @@ class Item < ApplicationRecord
   has_one_attached :image
   
   validates :image, :name, :info, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :price, :user, presence: true
+  validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 }
 
 end
