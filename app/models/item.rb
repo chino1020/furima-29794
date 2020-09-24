4f49814e47b64b65922f677c1c5eb3bcab9cfb8c
chino1020 <70316634+chino1020@users.noreply.github.com>
2020-09-24 17:42:42 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,6 @@ class Item < ApplicationRecord
 
   validates :image, :name, :info, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :price, :user, presence: true
   validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 }
+  validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999, :only_interger => true }
+  
 end
