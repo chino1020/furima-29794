f5006729fbd742786ba6bd0e778466474d33eb98
chino1020 <70316634+chino1020@users.noreply.github.com>
2020-09-25 11:18:15 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,16 @@ before_action :set_item, only: [:show, :destroy]
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def new
